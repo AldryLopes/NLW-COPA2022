1766b631e2b1b982bfd6f26e4b47d2dd50e9fd54
AldryLopes <aldrylopes@gmail.com>
2022-11-06 12:05:44 -0300

    Correção local Imagens

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+const isLocal = window.location.origin.startsWith('http://127')
+const projectName = '/Nlw-Copa2022'
+const imgPath = isLocal ? '' : projectName;
+
 function createGame(player1, title1, hour, player2, title2) {
   return `
   <li>
-        <img src="/assets/icon-${player1}.svg" alt="Bandeira do ${player1}" title="${title1}"/>
+        <img src="${imgPath}/assets/icon-${player1}.svg" alt="Bandeira do ${player1}" title="${title1}"/>
         <strong>${hour}</strong>
         <img src="/assets/icon-${player2}.svg" alt="Bandeira da ${player2}" title="${title2}" />
   </li>
